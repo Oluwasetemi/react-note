e9764f210a4010d697081cd64ea42c3999c5da3b
oluwasetemi ojo <setemiojo@gmail.com>
2026-05-04 11:55:11 -0500

    fix: use defineRoutesSetup in setup/routes.ts instead of defineAppSetup

---
+++ setup/routes.ts
@@ -0,0 +1,15 @@
+import { defineRoutesSetup } from '@slidev/types'
+
+export default defineRoutesSetup((routes) => {
+  return [
+    ...routes,
+    {
+      path: '/changelog',
+      component: () => import('../views/changelog.vue'),
+    },
+    {
+      path: '/routes',
+      component: () => import('../views/routes.vue'),
+    },
+  ]
+})
